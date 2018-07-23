a823238f004ea02a87f02b671565536e9608eada
Keshav Kumar <keshavkumar61996@gmail.com>
2018-07-23 19:49:40 +0530

    update db and controllers

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,24 @@
+class PostsController < ApplicationController
+    def index
+        @content_1="This i sample 1"
+        @content_2="This i sample 2"
+    end
+    def new
+    
+    end
+    def create
+    
+    end
+    def edit
+    
+    end
+    def update
+    
+    end
+    def show
+    
+    end
+    def destroy
+    
+    end
+end
