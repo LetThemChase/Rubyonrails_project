3f555b749c1ade62ab68397b90a6efee7889668e
Keshav Kumar <keshavkumar61996@gmail.com>
2018-07-31 06:49:31 +0530

    website look 1

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'pages/about'
+
+  get 'pages/contact'
+
+  get 'pages/resources'
+
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   get 'categories/index'
@@ -13,6 +19,13 @@ Rails.application.routes.draw do
 
     resources:posts
     resources:categories
+
+    get '/about', :to => 'pages#about'
+    get '/contact', :to => 'pages#contact'
+    get '/resources', :to => 'pages#resources'
+    #match '/about', :to => 'pages#about'
+    #match '/contact', :to => 'pages#contact'
+    #match '/resources', :to => 'pages#resources'
 
   root 'home#index' # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
