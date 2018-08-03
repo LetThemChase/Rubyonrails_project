f899909eaff9184bf8f4fa2ac0d83aacb296fc4d
Keshav Kumar <keshavkumar61996@gmail.com>
2018-08-03 23:28:29 +0530

    removed edit new view,commented these in controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,42 +6,41 @@ class PostsController < ApplicationController
         #@posts=Post.all
 
     end
-    def new
-        @post=Post.new
-        #@category =Category.all
-    end
-    def create
-        #@post=Post.new(params[:post])
-        @post=Post.new(post_params)
-        if @post.save 
-            redirect_to posts_path, :notice => "saved"
-        else 
-            render "new"
-        end
-    end
-
-
-
-    def edit
-        @post=Post.find(params[:id])
-    end
-    def update
-        @post=Post.find(params[:id])
-        if @post.update_attributes(post_params)
-            redirect_to post_path, :notice => "updated"
-        else 
-            render "edit"
-        end
-    end
     def show
         @post=Post.find(params[:id])
         @user=AdminUser.all
     end
-    def destroy
-        @post=Post.find(params[:id])
-        @post.destroy
-        redirect_to posts_path , :notice => "your post deleted"
-    end
+
+
+    #def new
+    #    @post=Post.new
+    #    #@category =Category.all
+    #end
+    #def create
+    #    #@post=Post.new(params[:post])
+    #    @post=Post.new(post_params)
+    #    if @post.save 
+    #        redirect_to posts_path, :notice => "saved"
+    #    else 
+    #        render "new"
+    #    end
+    #end
+    #def edit
+    #    @post=Post.find(params[:id])
+    #end
+    #def update
+    #    @post=Post.find(params[:id])
+    #    if @post.update_attributes(post_params)
+    #        redirect_to post_path, :notice => "updated"
+    #    else 
+    #        render "edit"
+    #    end
+    #end
+    #def destroy
+    #    @post=Post.find(params[:id])
+    #    @post.destroy
+    #    redirect_to posts_path , :notice => "your post deleted"
+    #end
 end
 
 def post_params
